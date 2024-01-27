0197592e497beb658174a7ca9469c74152dd362b
Myungsoo Son <thsaudtnx@gmail.com>
2024-01-27 11:36:31 +0900

    about navbar responive done

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,8 +5,12 @@ import Contents from './Contents';
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.name}>주식회사 YP뇌건강연구소</h1>
-      <Contents />
+      <h1 className={styles.name}>
+        주식회사 YP뇌건강연구소
+      </h1>
+      <div className={styles.contents}>
+        <Contents />
+      </div>
       <div className={styles.copyright}>
         Copyright (C) 주식회사 YP뇌건강연구소 All Rights Reserved.
       </div>
